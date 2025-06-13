dccd8cc1888c90164545cbed84780035fa8ea92c
Zyotiee111 <zyotieebista12@gmail.com>
2025-06-13 21:43:33 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
-import { auth } from "./firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
-import "./App.css";
-import NavBar from "./components/NavBar";
-import ChatBox from "./components/ChatBox";
-import Welcome from "./components/Welcome";
-import { useEffect, useRef } from "react";
+import { auth } from './firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import './App.css'
+import NavBar from './components/NavBar'
+import ChatBox from './components/ChatBox'
+import Welcome from './components/Welcome'
 
 function App() {
-  const [user] = useAuthState(auth);
-  const bubbleContainerRef = useRef(null);
-
-  useEffect(() => {
-    const bubbleContainer = bubbleContainerRef.current;
-
-    const createBubble = () => {
-      const bubble = document.createElement("div");
-      bubble.classList.add("bubble");
-      bubble.style.animationDuration = 5 + Math.random() * 10 + "s";
-      const size = 30 + Math.random() * 40;
-      bubble.style.width = bubble.style.height = `${size}px`;
-      bubble.style.left = Math.random() * (window.innerWidth - size) + "px";
-      bubbleContainer.appendChild(bubble);
-
-      setTimeout(() => {
-        bubble.remove();
-      }, 15000);
-    };
-
-    const interval = setInterval(createBubble, 500);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+  const [user] = useAuthState(auth)
 
   return (
     <div className="App" id="App">
-      <div className="bubble-container" ref={bubbleContainerRef}></div>
       <NavBar />
       {!user ? (
         <Welcome />
@@ -46,7 +19,7 @@ function App() {
         </>
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,28 +1,30 @@
-import React from "react";
-import GoogleSignin from "../img/btn_google_signin_dark_pressed_web.png";
-import { auth } from "../firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import React from 'react'
+import GoogleSignin from '../img/btn_google_signin_dark_pressed_web.png'
+import { auth } from '../firebase'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
 const Welcome = () => {
   const googleSignIn = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider);
-  };
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth, provider)
+  }
 
   return (
-    <main className="welcome">
-      <h1>Welcome to React Chat.</h1>
-      <p>Sign in with Google to chat with with your fellow React Developers.</p>
-      <button className="sign-in">
-        <img
-          onClick={googleSignIn}
-          src={GoogleSignin}
-          alt="sign in with google"
-          type="button"
-        />
-      </button>
-    </main>
-  );
-};
+    <div className="welcome-bar">
+      <main className="welcome">
+        <h1>Welcome to Pookie Chat.</h1>
+        <p>Sign in with Google to chat with with your Pookies.</p>
+        <button className="sign-in">
+          <img
+            onClick={googleSignIn}
+            src={GoogleSignin}
+            alt="sign in with google"
+            type="button"
+          />
+        </button>
+      </main>
+    </div>
+  )
+}
 
-export default Welcome;
+export default Welcome
